20758101285e46aee3e68910ef1ceb7eca410d2c
Maria <murikmarusia@gmail.com>
2019-03-06 17:34:48 +0200

    test deploy

---
+++ build/precache-manifest.1c2bd7b76bf52135a5fb31aa6291a2e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f6f564794bbc6f5017d",
+    "url": "/datatable_react/build/static/js/runtime~main.3f6f5647.js"
+  },
+  {
+    "revision": "9b6426eca90515a46772",
+    "url": "/datatable_react/build/static/js/main.9b6426ec.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/datatable_react/build/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "9b6426eca90515a46772",
+    "url": "/datatable_react/build/static/css/main.0c582372.chunk.css"
+  },
+  {
+    "revision": "def38b39e9ea0a0ec767de26fdba35eb",
+    "url": "/datatable_react/build/index.html"
+  }
+];
